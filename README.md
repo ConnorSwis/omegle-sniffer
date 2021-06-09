32c9f6c0edbecd1faef32a215f946f349db4e59c
ConnorSwis <connorswislow@gmail.com>
2021-06-09 15:00:32 -0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # omegle-sniffer
+
+An IP locator for Omegle.
+
+
+## Installation
+1. Clone the repository
+2. Install the required packages
+3. Run sniff.py
+
+## Usage
+Press `shift + a` to locate the IP.
+
+## Caveats
+It may not work for some people. It could be that the filter is blocking their IP prefix. Also, other traffic may interfere with the results, so if the Org is Google LLC or Valve Corp., make sure to block that IP prefix.
---
+++ src/sniff.py
@@ -0,0 +1,57 @@
+from pynput import keyboard
+import pyshark
+import requests
+from collections import Counter
+import os
+
+
+BLOCK_LIST = ['192', '162', '76', '31', '104']
+
+capture = pyshark.LiveCapture(
+    interface='Ethernet',
+    display_filter=f"udp && ip.src_host matches \"^(?!({'|'.join(BLOCK_LIST)})).*\"",
+    )
+
+
+def common(L):
+    if len(L) > 0:
+        return Counter(L).most_common(1)[0][0]
+    return None
+
+
+def sniff():
+    capture.sniff(timeout=1)
+    ip = common([packet.ip.src for packet in capture._packets])
+    if ip:
+        os.system('cls')
+        request = requests.get(f'http://ip-api.com/json/{str(ip)}')
+        json = request.json()
+        print('Country: ' + json['country'])
+        print('State: ' + json['regionName'])
+        print('City: ' + json['city'])
+        print('isp: ' + json['isp'])
+        print('Org: ' + json['org'])
+        print('IP: ' + json['query'])
+        capture.clear()
+
+
+cmb = [{keyboard.Key.shift, keyboard.KeyCode(char='a')},{keyboard.Key.shift, keyboard.KeyCode(char='A')}]
+  
+current = set()
+  
+def on_press(key):
+    if any([key in z for z in cmb]):
+        current.add(key)
+        if any(all(k in current for k in z) for z in cmb):
+            sniff()
+
+def on_release(key):
+    global current
+    if any([key in z for z in cmb]):
+        try:
+            current.remove(key)
+        except KeyError:
+            current = set()
+
+with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+  listener.join()
