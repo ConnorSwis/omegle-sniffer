abfa383476971a402bcca5dfc646ba1cb66b1e5b
ConnorSwis <condude13@gmail.com>
2022-01-20 00:26:35 -0500

    New Framework

--- README.md
+++ README.md
@@ -3,10 +3,11 @@
 An IP locator for Omegle.
 
 FOR EDUCATIONAL PURPOSES ONLY!
+I AM NOT RESPONSIBLE FOR ANY MISUSE OR ABUSE.
 
-I wanted a project to filter internet traffic and use an API.
+I wanted a project to filter internet traffic, use an API, and build an application window.
 
-![picture](https://raw.githubusercontent.com/ConnorSwis/omegle-sniffer/main/picture.png?token=AMO72IG6DR54RX3SXCEUJ5LAYEIBW)
+![picture](./picture.png)
 
 ## Installation
 1. Install tshark with [wireshark](https://www.wireshark.org/download.html)
@@ -15,7 +16,11 @@ I wanted a project to filter internet traffic and use an API.
 4. Run sniff.py
 
 ## Usage
-Press `shift + a` to locate the IP.
+Press the button to automatically locate the IP.
 
 ## Caveats
 It may not work for some people. It could be that the filter is blocking their IP prefix. Also, other traffic may interfere with the results, so if the Org is Google LLC or Valve Corp., make sure to block that IP prefix.
+
+
+## How it works
+Using a [pyshark](https://pypi.org/project/pyshark/), a python wrapper for tshark, this program captures traffic for 1 second and locates the most common IP address that appears. This can lead to incorrect IPs being selected to be searched. The block list allows for the user to block IP addresses which start with any number. [PyQt6](https://pypi.org/project/PyQt6/) was used to make the application window.
--- src/sniff.py
+++ src/sniff.py
@@ -1,56 +1,82 @@
-from pynput import keyboard
+import sys
+from collections import Counter
+
 import pyshark
 import requests
-from collections import Counter
-import os
+from PyQt6 import QtGui
+from PyQt6.QtWidgets import (QApplication, QLabel, QMainWindow, QPushButton,
+                             QVBoxLayout, QWidget)
 
-BLOCK_LIST = ['192', '76', '31', '104', '24']
+BLOCK_LIST = ['31', '192', '75', '10', '162', '73']
 
-capture = pyshark.LiveCapture(
-    interface='Ethernet',
-    display_filter=f"udp && ip.src_host matches \"^(?!({'|'.join(BLOCK_LIST)})).*\"",
-    )
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.block_list = BLOCK_LIST
+
+        self.capture = pyshark.LiveCapture(
+            interface='Ethernet',
+            display_filter=(
+                "udp && ip.src_host matches" +
+                " \"^(?!({'|'.join(self.block_list)})).*\""
+            ),
+        )
+
+        self.setWindowTitle("Sniffer")
+
+        self.button = QPushButton("Sniff!")
+        self.button.setCheckable(True)
+        self.button.clicked.connect(self.sniff)
+
+        self.label = QLabel(
+            '\n'.join((
+                'Country: ', 'State: ', 'City: ',
+                'isp: ', 'Org: ', 'IP: '
+            ))
+        )
+        self.button.setFont(QtGui.QFont('Arial', 11))
+        self.label.setFont(QtGui.QFont('Arial', 11))
+
+        layout = QVBoxLayout()
+        layout.addWidget(self.label)
+        layout.addWidget(self.button)
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setFixedSize(210, 150)
+        self.setWindowIcon(QtGui.QIcon('./src/sniffer.png'))
+        self.setCentralWidget(widget)
+
+    @staticmethod
+    def common(L):
+        if len(L) > 0:
+            return Counter(L).most_common(1)[0][0]
+        return None
 
 
-def common(L):
-    if len(L) > 0:
-        return Counter(L).most_common(1)[0][0]
-    return None
+    def sniff(self):
+        self.capture.sniff(timeout=1)
+        if len(self.capture._packets) > 0:
+            ip = Counter(
+                [packet.ip.src for packet in self.capture._packets]
+            ).most_common(1)[0][0]
+            if ip:
+                request = requests.get(f'http://ip-api.com/json/{str(ip)}')
+                json = request.json()
+                text = '\n'.join((
+                    'Country: ' + json.get('country', ''),
+                    'State: ' + json.get('regionName', ''),
+                    'City: ' + json.get('city', ''),
+                    'isp: ' + json.get('isp', ''),
+                    'Org: ' + json.get('org', ''),
+                    'IP: ' + json.get('query', '')
+                ))
+                self.capture.clear()
+                self.label.setText(text)
 
 
-def sniff():
-    capture.sniff(timeout=1)
-    ip = common([packet.ip.src for packet in capture._packets])
-    if ip:
-        os.system('cls')
-        request = requests.get(f'http://ip-api.com/json/{str(ip)}')
-        json = request.json()
-        print('Country: ' + json.get('country', ''))
-        print('State: ' + json.get('regionName', ''))
-        print('City: ' + json.get('city', ''))
-        print('isp: ' + json.get('isp', ''))
-        print('Org: ' + json.get('org', ''))
-        print('IP: ' + json.get('query', ''))
-        capture.clear()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
 
-
-cmb = [{keyboard.Key.shift, keyboard.KeyCode(char='a')},{keyboard.Key.shift, keyboard.KeyCode(char='A')}]
-  
-current = set()
-  
-def on_press(key):
-    if any([key in z for z in cmb]):
-        current.add(key)
-        if any(all(k in current for k in z) for z in cmb):
-            sniff()
-
-def on_release(key):
-    global current
-    if any([key in z for z in cmb]):
-        try:
-            current.remove(key)
-        except KeyError:
-            current = set()
-
-with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-  listener.join()
+    window = MainWindow()
+    window.show()
+    app.exec()
